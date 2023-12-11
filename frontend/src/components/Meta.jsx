27a1671888eb8222a99d267221b99c8322989f6e
Allen <rockenmyvibe@Allens-MacBook-Air.local>
2023-12-10 19:05:10 -0600

    4th commit

--- frontend/src/components/Meta.jsx
+++ frontend/src/components/Meta.jsx
@@ -11,9 +11,9 @@ const Meta = ({ title, description, keywords }) => {
 };
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Rocken My Vibe',
+  description: 'High Quality appreal for e-commerce website',
+  keywords: 'Hoodies, T-shirts, Coffee Mugs, Hats, Pillows, Water Bottles, Tumblers, sweatshirts',
 };
 
 export default Meta;
